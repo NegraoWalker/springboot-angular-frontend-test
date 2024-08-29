f07a7cb94660365c2dcfa74b3c64fbba14435e30
walkerNegrão <walker.esteves.negrao@gmail.com>
2024-08-29 18:42:34 -0300

    Validação dos componentes e chamadas para o backend

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { ActivatedRoute } from '@angular/router';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-details',
   templateUrl: './employee-details.component.html',
   styleUrls: ['./employee-details.component.css']
 })
-export class EmployeeDetailsComponent {
+export class EmployeeDetailsComponent implements OnInit {
 
+  id!: number;
+  employee: Employee = new Employee();
+  constructor(private route: ActivatedRoute, private employeService: EmployeeService) { }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.employee = new Employee();
+    this.employeService.getEmployeeById(this.id).subscribe(data => {
+      this.employee = data;
+    });
+  }
 }
